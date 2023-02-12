1bcc2876f3c78a247746ab0adf28622820aa8056
Santiago Garcia Santos <santiago@garciasantos.org>
2023-02-12 13:09:47 -0500

    Added city.ts type

---
+++ client/src/util/types/city.ts
@@ -0,0 +1,12 @@
+/**
+ * Interface for the city data type return from the backend
+ */
+interface ICity {
+  _id: string;
+  cityName: string;
+  isAccredited: boolean;
+  countiesCovered: [string];
+  indicators: Map<string, Map<string, number>>;
+}
+
+export default ICity;
