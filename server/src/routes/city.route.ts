1141ec10f79d707a09eac77ca0fb96998b8e43d6
tarakapoor <tarakapoor9@gmail.com>
2023-02-12 14:10:12 -0500

    indicator data route in city files task

--- server/src/routes/city.route.ts
+++ server/src/routes/city.route.ts
@@ -1,10 +1,19 @@
 import express from 'express';
-import { getAllCities, getCity } from '../controllers/city.controller';
+import {
+  getAllCities,
+  getCity,
+  getIndicator,
+} from '../controllers/city.controller';
 
 const router = express.Router();
 
 router.get('/all', getAllCities);
 
 router.get('/:cityName', getCity);
+
+/**
+ * A GET route to get city indicator data
+ */
+router.get('/indicator/:indicatorName', getIndicator); //isAuthenticated, isAdmin, getIndicator);
 
 export default router;
