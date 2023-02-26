f3e9a18beee107761f1f115c94814abfbbbbc067
BEW111 <bewilliams111@gmail.com>
2023-02-26 14:16:14 -0500

    added counties

--- client/src/components/widgets/CityNameWidget.tsx
+++ client/src/components/widgets/CityNameWidget.tsx
@@ -14,65 +14,73 @@ type DefaultWidgetProps = {
 function CityNameWidget({ city }: DefaultWidgetProps) {
   const cityData = useData(`cities/${city}`);
 
-  const accredited = cityData?.data.isAccredited;
-  const established = 2012;
-  // const established = cityData?.data.established;
-  const populationList: { [key: number]: number } =
-    cityData?.data.indicators.population;
-  const maleParticipantsList: { [key: number]: number } =
-    cityData?.data.indicators.male_participants;
-  const femaleParticipantsList: { [key: number]: number } =
-    cityData?.data.indicators.female_participants;
+  if (cityData) {
+    const accredited = cityData?.data.isAccredited;
+    const counties = cityData?.data.countiesCovered;
+    const established = 2012;
+    // const established = cityData?.data.established;
+    const populationList: { [key: number]: number } =
+      cityData?.data.indicators.population;
+    const maleParticipantsList: { [key: number]: number } =
+      cityData?.data.indicators.male_participants;
+    const femaleParticipantsList: { [key: number]: number } =
+      cityData?.data.indicators.female_participants;
 
-  const population =
-    Object.values(populationList)[Object.values(populationList).length - 1];
-  const participants =
-    Object.values(maleParticipantsList)[
-      Object.values(maleParticipantsList).length - 1
-    ] +
-    Object.values(femaleParticipantsList)[
-      Object.values(femaleParticipantsList).length - 1
-    ];
+    const population =
+      Object.values(populationList)[Object.values(populationList).length - 1];
+    const participants =
+      Object.values(maleParticipantsList)[
+        Object.values(maleParticipantsList).length - 1
+      ] +
+      Object.values(femaleParticipantsList)[
+        Object.values(femaleParticipantsList).length - 1
+      ];
 
-  return (
-    <Paper elevation={1} sx={{ position: 'fixed', p: 3 }}>
-      <Box sx={{ display: 'flex', justifyContent: 'space-between', mr: 1 }}>
-        <Box>
-          <Typography variant="h4">{city}</Typography>
-          <Typography variant="subtitle2" sx={{ color: COLORS.gray, mb: 10 }}>
-            {accredited && 'Accredited'}
-          </Typography>
+    return (
+      <Paper elevation={1} sx={{ position: 'fixed', p: 3 }}>
+        <Box sx={{ display: 'flex', justifyContent: 'space-between', mr: 1 }}>
+          <Box>
+            <Typography variant="h4">{city}</Typography>
+            <Typography variant="subtitle2" sx={{ color: COLORS.gray }}>
+              {accredited && 'Accredited'}
+            </Typography>
+            <Typography variant="subtitle2" sx={{ color: COLORS.gray, mb: 10 }}>
+              {counties.join(', ')}
+            </Typography>
+          </Box>
+          <img src={PhiladelphiaLogo} alt="logo" width={64} height={64} />
         </Box>
-        <img src={PhiladelphiaLogo} alt="logo" width={64} height={64} />
-      </Box>
-      <Box sx={{ display: 'flex', gap: 4 }}>
-        <Box>
-          <Typography variant="subtitle1">Population</Typography>
-          <Typography variant="h5" sx={{ fontWeight: 700 }}>
-            {Intl.NumberFormat('en-US', {
-              notation: 'compact',
-              maximumFractionDigits: 1,
-            }).format(population)}
-          </Typography>
+        <Box sx={{ display: 'flex', gap: 4 }}>
+          <Box>
+            <Typography variant="subtitle1">Population</Typography>
+            <Typography variant="h5" sx={{ fontWeight: 700 }}>
+              {Intl.NumberFormat('en-US', {
+                notation: 'compact',
+                maximumFractionDigits: 1,
+              }).format(population)}
+            </Typography>
+          </Box>
+          <Box>
+            <Typography variant="subtitle1">Participants</Typography>
+            <Typography variant="h5" sx={{ fontWeight: 700 }}>
+              {Intl.NumberFormat('en-US', {
+                notation: 'compact',
+                maximumFractionDigits: 1,
+              }).format(participants)}
+            </Typography>
+          </Box>
+          <Box>
+            <Typography variant="subtitle1">Established</Typography>
+            <Typography variant="h5" sx={{ fontWeight: 700 }}>
+              {established}
+            </Typography>
+          </Box>
         </Box>
-        <Box>
-          <Typography variant="subtitle1">Participants</Typography>
-          <Typography variant="h5" sx={{ fontWeight: 700 }}>
-            {Intl.NumberFormat('en-US', {
-              notation: 'compact',
-              maximumFractionDigits: 1,
-            }).format(participants)}
-          </Typography>
-        </Box>
-        <Box>
-          <Typography variant="subtitle1">Established</Typography>
-          <Typography variant="h5" sx={{ fontWeight: 700 }}>
-            {established}
-          </Typography>
-        </Box>
-      </Box>
-    </Paper>
-  );
+      </Paper>
+    );
+  }
+
+  return <Paper elevation={1} sx={{ position: 'fixed', p: 3 }} />;
 }
 
 export default CityNameWidget;
