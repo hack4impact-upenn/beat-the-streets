42ad6ccec9cc2cb84a284dc80c86d30d0073471f
Caroline Chen <carolinechen@Carolines-MacBook-Air.local>
2023-03-07 23:24:32 +0000

    main dashboard

--- client/src/MainDashboard/MainDashboardPage.tsx
+++ client/src/MainDashboard/MainDashboardPage.tsx
@@ -3,11 +3,36 @@ import { Masonry } from '@mui/lab';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import { Typography, Grid, Toolbar } from '@mui/material';
+import { useData } from '../util/api';
 
 const heights = [
   150, 80, 90, 70, 110, 150, 130, 200, 60, 90, 150, 80, 90, 200, 110, 150, 130,
   80, 60, 90,
 ];
+
+function createCityCard(city: any) {
+  const name = city.cityName;
+  const status = city.isAccredited;
+  if (status) {
+    return (
+      <Paper sx={{ p: 2 }}>
+        <Typography variant="h4">{name}</Typography>
+        <Typography>Accredited</Typography>
+      </Paper>
+    );
+  } else {
+    return (
+      <Paper sx={{ p: 2 }}>
+        <Typography variant="h4">{name}</Typography>
+        <Typography>Not Accredited</Typography>
+      </Paper>
+    );
+  }
+}
+
+function createData(cityData: any) {
+  return cityData.map(createCityCard);
+}
 
 /**
  * The Graph Page of the main dashboard.
@@ -18,6 +43,26 @@ function MainDashboard() {
     document.body.style.backgroundColor = 'lightgray';
   });
 
+  //get all of the city names
+  //put city name and accreditation in component
+  const cities = useData(`cities/all`);
+  let cityData = cities?.data;
+  const statuses: string[] = [];
+
+  if (cityData) {
+    for (let i = 0; i < cityData.length; i++) {
+      const name = cityData[i].cityName;
+      const status = cityData[i].isAccredited;
+      if (status) {
+        statuses.push('Accredited');
+      } else {
+        statuses.push('Not Accredited');
+      }
+    }
+  } else {
+    cityData = [];
+  }
+
   return (
     <Box
       component="main"
@@ -26,11 +71,10 @@ function MainDashboard() {
       <Toolbar />
 
       <Grid container spacing={4}>
-        <Grid item xs={4}>
-          <Paper elevation={1} sx={{ position: 'fixed', p: 3 }}>
-            <Typography variant="h4">Philadelphia</Typography>
-            <Typography>Accredited</Typography>
-          </Paper>
+        <Grid item xs={3}>
+          <Masonry columns={1} spacing={4}>
+            {createData(cityData)}
+          </Masonry>
         </Grid>
         <Grid item xs={8}>
           <Masonry columns={3} spacing={4}>
