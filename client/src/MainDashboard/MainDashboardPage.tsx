7d63e0b4d22b2469f086d0610bfd817e1236a123
Caroline Chen <carolinechen@brb-02349.apn.wlan.private.upenn.edu>
2023-03-29 18:27:33 -0400

    initial work

--- client/src/MainDashboard/MainDashboardPage.tsx
+++ client/src/MainDashboard/MainDashboardPage.tsx
@@ -1,9 +1,10 @@
-import React, { useLayoutEffect } from 'react';
+import React, { useState, useLayoutEffect } from 'react'
 import { Masonry } from '@mui/lab';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import { Typography, Grid, Toolbar } from '@mui/material';
 import { useData } from '../util/api';
+import SplitPane, { Pane } from 'split-pane-react';
 
 const heights = [
   150, 80, 90, 70, 110, 150, 130, 200, 60, 90, 150, 80, 90, 200, 110, 150, 130,
@@ -48,6 +49,8 @@ function MainDashboard() {
   let cityData = cities?.data;
   const statuses: string[] = [];
 
+  const [sizes, setSizes] = useState([100, '30%', 'auto']);
+
   if (cityData) {
     for (let i = 0; i < cityData.length; i += 1) {
       const status = cityData[i].isAccredited;
@@ -67,23 +70,31 @@ function MainDashboard() {
       sx={{ flexGrow: 1, paddingTop: 4, paddingLeft: 4, width: { sm: '100%' } }}
     >
       <Toolbar />
+      <SplitPane
+        split='vertical'
+        sizes={sizes}
+        onChange={setSizes}
+      >
+        <Pane>
+          <Grid item xs={3}>
+            <Masonry columns={1} spacing={4}>
+              {createData(cityData)}
+            </Masonry>
+          </Grid>
+        </Pane>
 
-      <Grid container spacing={4}>
-        <Grid item xs={3}>
-          <Masonry columns={1} spacing={4}>
-            {createData(cityData)}
-          </Masonry>
-        </Grid>
-        <Grid item xs={8}>
-          <Masonry columns={3} spacing={4}>
-            {heights.map((height) => (
-              <Paper elevation={0} sx={{ height, p: 3 }}>
-                <Typography>add graph here</Typography>
-              </Paper>
-            ))}
-          </Masonry>
-        </Grid>
-      </Grid>
+        <Pane>
+          <Grid item xs={8}>
+            <Masonry columns={3} spacing={4}>
+              {heights.map((height) => (
+                <Paper elevation={0} sx={{ height, p: 3 }}>
+                  <Typography>add graph here</Typography>
+                </Paper>
+              ))}
+            </Masonry>
+          </Grid>
+        </Pane>
+      </SplitPane>
     </Box>
   );
 }
