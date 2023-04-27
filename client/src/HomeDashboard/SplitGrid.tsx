4a364b23284e53daba94a2449b50c3e2c7c9fb14
annabay04 <annabay2004@gmail.com>
2023-04-27 16:10:59 -0400

    make buttons, routes, added components

--- client/src/HomeDashboard/SplitGrid.tsx
+++ client/src/HomeDashboard/SplitGrid.tsx
@@ -3,32 +3,61 @@ import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import { styled } from '@mui/system';
 import Toolbar from '@mui/material/Toolbar';
-import { Typography } from '@mui/material';
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  Grid,
+  Typography,
+} from '@mui/material';
+import { Masonry } from '@mui/lab';
+import { useNavigate } from 'react-router-dom';
 import { useData } from '../util/api';
+import TotalChapters from '../components/indicatorComponents/TotalChapters';
+import TotalCoaches from '../components/indicatorComponents/TotalCoaches';
+import TotalParticipants from '../components/indicatorComponents/TotalParticipants';
+import LineComponent from '../components/LineComponent';
+import CityCard from '../components/buttons/CityCard';
 
 const ScrollableBox = styled(Box)({
   overflowY: 'auto',
   maxHeight: '100%',
 });
 
+const heights = [
+  150, 80, 90, 70, 110, 150, 130, 200, 60, 90, 150, 80, 90, 200, 110, 150, 130,
+  80, 60, 90,
+];
+
 // eslint-disable-next-line
 function createCityCard(city: any) {
   const name = city.cityName;
   const status = city.isAccredited;
-  if (status) {
-    return (
-      <Paper sx={{ p: 2, bgcolor: '#EDEDED', mb: 2 }}>
-        <Typography variant="h5">{name}</Typography>
-        <Typography variant="subtitle1">Accredited</Typography>
-      </Paper>
-    );
-  }
-  return (
-    <Paper sx={{ p: 2, bgcolor: '#EDEDED', mb: 2 }}>
-      <Typography variant="h5">{name}</Typography>
-      <Typography variant="subtitle1">Not Accredited</Typography>
-    </Paper>
-  );
+  return <CityCard cityName={name} accredited={status} />;
+
+  // if (status) {
+  //   return (
+  //     <CityCard cityName={name} accredited={t} } />
+  //     // <Card sx={{ p: 2, bgcolor: '#EDEDED', mb: 2 }}>
+  //     //   <CardActionArea>
+  //     //     <CardContent>
+  //     //       <Typography variant="h5">{name}</Typography>
+  //     //       <Typography variant="subtitle1">Accredited</Typography>
+  //     //     </CardContent>
+  //     //   </CardActionArea>
+  //     // </Card>
+  //   );
+  // }
+  // return (
+  //   <Card sx={{ p: 2, bgcolor: '#EDEDED', mb: 2 }}>
+  //     <CardActionArea>
+  //       <CardContent onClick={() => console.log('Clicked!')}>
+  //         <Typography variant="h5">{name}</Typography>
+  //         <Typography variant="subtitle1">Not Accredited</Typography>
+  //       </CardContent>
+  //     </CardActionArea>
+  //   </Card>
+  // );
 }
 
 // eslint-disable-next-line
@@ -42,7 +71,7 @@ function SplitGrid() {
 
   return (
     <Box display="flex" flexDirection="column" width="100%" height="100vh">
-      <Toolbar>{/* Add your toolbar content here */}</Toolbar>
+      <Toolbar />
 
       <Box display="flex" flexGrow={1}>
         <Paper
@@ -72,11 +101,20 @@ function SplitGrid() {
           square
         >
           <h2>National Statistics</h2>
-          <div>
-            {Array.from({ length: 50 }, (_, i) => (
-              <p key={i}>Right Column Content {i + 1}</p>
-            ))}
-          </div>
+          <Grid item xs={8}>
+            <Masonry columns={3} spacing={4}>
+              <TotalChapters />
+              <TotalCoaches />
+              <TotalParticipants />
+              <LineComponent variant="revenue" />
+              <LineComponent variant="expenses" />
+              {/* {heights.map((height) => (
+                <Paper elevation={0} sx={{ height, p: 3 }}>
+                  <Typography>add graph here</Typography>
+                </Paper>
+              ))} */}
+            </Masonry>
+          </Grid>
         </Paper>
       </Box>
     </Box>
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
 import { AlertProvider } from './util/context/AlertContext';
